ef509ebd9ea0e419c27f6274811365320474f638
Rauf <zen.star12@yahoo.com>
2021-02-07 02:32:16 +0700

    Tugas 9 Javascript

---
+++ tugas9.js
@@ -0,0 +1,11 @@
+function tugas9(){
+    var siswa = {
+        Nama    : 'Rauf',
+        Batch   : 3,
+        JK      : 'Pria'
+    }
+        for(var x in siswa){
+            console.log(siswa[x])
+        }
+}
+tugas9()
